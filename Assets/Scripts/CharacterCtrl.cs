7b78a09d911eb3de8eff95ba6149355febf985e7
okabe783 <134473737+okabe783@users.noreply.github.com>
2024-04-02 11:01:44 +0900

    [Add Animator] ComboAttack

--- Assets/Scripts/CharacterCtrl.cs
+++ Assets/Scripts/CharacterCtrl.cs
@@ -7,14 +7,18 @@ public class CharacterCtrl : MonoBehaviour
     private Animator _animator;
     private characterInput _input;
     private bool _inAttack; //攻撃をしているかの判定
+
     private bool _inCombo; //連続攻撃をしているかの判定
+
     //AnimatorCtrlの現在の状態や進行状況
     private AnimatorStateInfo _previousCurrentStateInfo;
     private AnimatorStateInfo _currentStateInfo;
     private AnimatorStateInfo _nextStateInfo;
+    private bool _isAnimatorTransitioning; //トランジション中か判定
 
     //Hash値
     private readonly int _hashWeaponAttack = Animator.StringToHash("Attack");
+    private readonly int _hashStateTime = Animator.StringToHash("StateTime");
     private readonly int _hashFirstCombo = Animator.StringToHash("Combo1");
     private readonly int _hashSecondCombo = Animator.StringToHash("Combo2");
     private readonly int _hashThirdCombo = Animator.StringToHash("Combo3");
@@ -26,9 +30,20 @@ public class CharacterCtrl : MonoBehaviour
         _weapon.SetOwner(gameObject);
     }
 
+    private void OnEnable()
+    {
+        EquipWeapon(false);
+    }
+
     private void FixedUpdate()
     {
+        CacheAnimatorState();
         EquipWeapon(IsWeaponEquip());　//武器を装備
+        //StateTimeを0から1の範囲でループさせる
+        _animator.SetFloat(_hashStateTime, Mathf.Repeat(_animator.GetCurrentAnimatorStateInfo(0)
+            .normalizedTime, 1f));
+        _animator.ResetTrigger(_hashWeaponAttack); //Triggerをreset
+        //攻撃Animを再生
         if (_input.Attack && canAttack)
             _animator.SetTrigger(_hashWeaponAttack);
     }
@@ -40,6 +55,15 @@ public class CharacterCtrl : MonoBehaviour
         _inCombo = equip;
         if (!equip)
             _animator.ResetTrigger(_hashWeaponAttack); //装備解除時にTriggerをReset
+    }
+
+    /// <summary>Animatorの状態をcache</summary>
+    private void CacheAnimatorState()
+    {
+        _previousCurrentStateInfo = _currentStateInfo;
+        _currentStateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+        _nextStateInfo = _animator.GetNextAnimatorStateInfo(0);
+        _isAnimatorTransitioning = _animator.IsInTransition(0);
     }
 
     /// <summary>武器がアクティブになっているかの判定</summary>
